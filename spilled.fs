2c19a1b93ad66b60facb97a6683c782ea3f7c3c2
Nate Whetsell <nathan.whetsell@gmail.com>
2025-08-06 12:17:27 -0400

    Refactor

--- spilled.fs
+++ spilled.fs
@@ -134,6 +134,29 @@
 //   lightPhi = acos(2 / lightRadius) ≈ 35.2643896828°
 //   lightTheta = atan2(1, 1) = atan(1) = pi / 4 = 45°
 
+
+// Constants and functions from LYGIA <https://github.com/patriciogonzalezvivo/lygia>
+#define PI 3.1415926535897932384626433832795
+#define TWO_PI 6.2831853071795864769252867665590
+#define DEG2RAD (PI / 180.0)
+
+vec2 polar2cart(in vec2 polar) {
+    return vec2(cos(polar.x), sin(polar.x)) * polar.y;
+}
+
+vec3 polar2cart( in float r, in float phi, in float theta) {
+    float x = r * cos(theta) * sin(phi);
+    float y = r * sin(theta) * sin(phi);
+    float z = r * cos(phi);
+    return vec3(x, y, z);
+}
+
+mat2 rotate2d(const in float r) {
+    float c = cos(r);
+    float s = sin(r);
+    return mat2(c, s, -s, c);
+}
+
 // Hash function from <https://www.shadertoy.com/view/4djSRW>, MIT-licensed:
 //
 // Copyright © 2014 David Hoskins.
@@ -166,8 +189,6 @@ float hash11(float p)
 
 // #define SUPPORT_EVEN_ROTNUM
 
-#define PI 3.1415926535897932384626433832795
-
 void main()
 {
     vec2 pos = gl_FragCoord.xy;
@@ -177,17 +198,15 @@ void main()
     if (PASSINDEX == 0) // ShaderToy Buffer A
     {
         int RotNum = 2 * int(agitation) + 1;
-        float ang = 2. * PI / float(RotNum);
-        mat2 m = mat2( cos(ang), sin(ang),
-                      -sin(ang), cos(ang));
+        float ang = TWO_PI / float(RotNum);
+        mat2 m = rotate2d(ang);
 #ifdef SUPPORT_EVEN_ROTNUM
-        mat2 mh = mat2( cos(ang * 0.5), sin(ang * 0.5),
-                       -sin(ang * 0.5), cos(ang * 0.5));
+        mat2 mh = rotate2d(ang * 0.5);
 #endif
 
         float rnd = hash11(TIME / RENDERSIZE.x);
-        vec2 b = vec2(cos(ang * rnd), sin(ang * rnd));
-        vec2 v = vec2(0.);
+        vec2 b = polar2cart(vec2(ang * rnd, 1));
+        vec2 v = vec2(0);
 
         float bbMax = dripSize * RENDERSIZE.y;
         bbMax *= bbMax;
@@ -226,7 +245,7 @@ void main()
                                           ; _++) {
                     rot += dot(
                         IMG_NORM_PIXEL(mainPass, fract((pos_plus_p + rotated_b) / RENDERSIZE)).xy - vec2(0.5),
-                        rotated_b.yx * vec2(1., -1.)
+                        rotated_b.yx * vec2(1, -1)
                     );
                     rotated_b = m * rotated_b;
                 }
@@ -239,9 +258,9 @@ void main()
             b *= 2.;
         }
 
-        gl_FragColor = IMG_NORM_PIXEL(mainPass, fract((pos + fluidSpeed * vec2(-1., 1.) * v) / RENDERSIZE));
+        gl_FragColor = IMG_NORM_PIXEL(mainPass, fract((pos + fluidSpeed * vec2(-1, 1) * v) / RENDERSIZE));
 
-        // add a little "motor" in the center
+        // add a little "motor"
         vec2 scr = 2. * (uv - motorLocation);
         gl_FragColor.xy += motorSize * scr / (10. * dot(scr, scr) + motorAttenuation);
 
@@ -253,7 +272,7 @@ void main()
     }
     else // ShaderToy Image
     {
-        vec2 d = vec2(inverseSize.y, 0.);
+        vec2 d = vec2(inverseSize.y, 0);
         vec3 n = vec3(
             (length(IMG_NORM_PIXEL(mainPass, uv + d.xy).xyz) - length(IMG_NORM_PIXEL(mainPass, uv - d.xy).xyz)) * RENDERSIZE.y,
             (length(IMG_NORM_PIXEL(mainPass, uv + d.yx).xyz) - length(IMG_NORM_PIXEL(mainPass, uv - d.yx).xyz)) * RENDERSIZE.y,
@@ -269,16 +288,9 @@ void main()
 
         n = normalize(spread_n);
 
-#define DEG2RAD (PI / 180.0)
-        float lightThetaInRadians = lightTheta * DEG2RAD;
-        float lightPhiInRadians = lightPhi * DEG2RAD;
-        vec3 light = normalize(lightRadius * vec3(
-            cos(lightThetaInRadians) * sin(lightPhiInRadians),
-            sin(lightThetaInRadians) * sin(lightPhiInRadians),
-            cos(lightPhiInRadians)
-        ));
+        vec3 light = normalize(polar2cart(lightRadius, lightPhi * DEG2RAD, lightTheta * DEG2RAD));
         float diff = clamp(dot(n, light), 0.5, 1.);
-        float spec = clamp(dot(reflect(light, n), vec3(0., 0., -1.)), 0., 1.);
+        float spec = clamp(dot(reflect(light, n), vec3(0, 0, -1)), 0., 1.);
         spec = pow(spec, 36.) * 2.5;
 
     	gl_FragColor = IMG_NORM_PIXEL(mainPass, uv) * vec4(diff) + specularReflectionAmount * vec4(spec);
